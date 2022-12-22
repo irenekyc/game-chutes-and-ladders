374c1973ab6674ee5d201bc41be9282c07c7e571
irenekyc <irene@usemanifest.com>
2022-12-21 18:14:04 -0600

    added player pieces

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import "./index.scss";
 import ladder from "./assets/ladder.png";
 import snake from "./assets/snake.svg";
+import battleship from "./assets/pieces/piece_battleship.png";
+import car from "./assets/pieces/piece_car.png";
+import dog from "./assets/pieces/piece_dog.png";
+import cat from "./assets/pieces/piece_cat.png";
+import hat from "./assets/pieces/piece_hat.png";
+import shoe from "./assets/pieces/piece_shoe.png";
+import wheelbarrow from "./assets/pieces/piece_wheelbarrow.png";
+
 const stepWithLadder = [1, 4, 8, 21, 28, 50, 71, 80];
 const stepWithSnakes = [32, 36, 48, 62, 88, 95, 97];
 
 function App() {
   return (
     <div className="app">
-      <div className="app__header">Manifest Chutes and Ladders</div>
+      <div className="app__header">
+        <h1>Manifest Chutes and Ladders</h1>
+      </div>
       <div className="app__main">
         <div className="board__wrapper">
           <div className="board">
@@ -52,8 +62,45 @@ function App() {
           </div>
         </div>
         <div className="tools">
-          <div className="tools__current">Current Player | Dice</div>
-          <div className="tools__players">Players</div>
+          <div className="tools__current">
+            <div className="tools__current__player">
+              <img src={battleship} alt="" />
+              <div className="tools__current__player__colorcode brown"></div>
+              <div className="tools__current__player__name">Anuraag</div>
+            </div>
+            <div className="tools__current__player">
+              <img src={car} alt="" />
+              <div className="tools__current__player__colorcode orange"></div>
+              <div className="tools__current__player__name">Rick</div>
+            </div>
+            <div className="tools__current__player">
+              <img src={cat} alt="" />
+              <div className="tools__current__player__colorcode yellow"></div>
+              <div className="tools__current__player__name">Meenakshi</div>
+            </div>
+            <div className="tools__current__player">
+              <img src={shoe} alt="" />
+              <div className="tools__current__player__colorcode green"></div>
+              <div className="tools__current__player__name">Ian</div>
+            </div>
+            <div className="tools__current__player">
+              <img src={dog} alt="" />
+              <div className="tools__current__player__colorcode light-green"></div>
+              <div className="tools__current__player__name">Riley</div>
+            </div>
+            <div className="tools__current__player">
+              <img src={hat} alt="" />
+              <div className="tools__current__player__colorcode blue"></div>
+              <div className="tools__current__player__name">Sabrina</div>
+            </div>
+            <div className="tools__current__player">
+              <img src={wheelbarrow} alt="" />
+              <div className="tools__current__player__colorcode purple"></div>
+              <div className="tools__current__player__name">Irene</div>
+            </div>
+          </div>
+
+          <div className="tools__players"></div>
           <div className="tools__history">Log</div>
         </div>
       </div>
